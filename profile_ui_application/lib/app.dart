2a4f039a3b7f5aff15ec5e63a7e3f05d615f1a79
Sidkjr <siddhant.khare.sk@gmail.com>
2023-05-07 19:34:58 +0530

    Finished with a basic UI for a Profile

--- profile_ui_application/lib/app.dart
+++ profile_ui_application/lib/app.dart
@@ -4,33 +4,58 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme:
-          ThemeData(appBarTheme: const AppBarTheme(color: Colors.greenAccent)),
+      theme: ThemeData(
+          appBarTheme:
+              const AppBarTheme(color: Color.fromARGB(255, 208, 47, 2))),
       home: Scaffold(
           appBar: AppBar(
             title: const Text("Profile UI"),
           ),
-          body: Column(
-            children: <Widget>[
-              Image.asset("images/madara.jpg", width: 200, height: 200),
-              const ListTile(
-                title: Text("Madara Uchiha"),
-                subtitle: Text("The Ghost of Uchiha"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Message"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Fight Me"),
-              ),
-              const ListTile(
-                title: Text("About Me"),
-                subtitle: Text(
-                    "An Elite Ninja, Brother of Hashirama who was the first Hokage of the village hidden in the leaves."),
-              )
-            ],
+          body: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.red[300]!, Colors.red[50]!])),
+            child: Column(
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage("images/madara.jpg"),
+                ),
+                const ListTile(
+                  title: Center(
+                      child: Text(
+                    "Madara Uchiha",
+                    style: TextStyle(fontSize: 20),
+                  )),
+                  subtitle: Center(
+                      child: Text(
+                    "The Ghost of Uchiha",
+                    style: TextStyle(fontSize: 15),
+                  )),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Message"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Fight Me"),
+                    ),
+                  ],
+                ),
+                const ListTile(
+                  title: Text("About Me", style: TextStyle(fontSize: 20)),
+                  subtitle: Text(
+                      "An Elite Ninja, Brother of Hashirama who was the first Hokage of the village hidden in the leaves.",
+                      style: TextStyle(fontSize: 15)),
+                )
+              ],
+            ),
           )),
     );
   }
