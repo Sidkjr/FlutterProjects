79639683f3f4d4d07bc994f145a9b8690a15dcb9
Sidkjr <siddhant.khare.sk@gmail.com>
2023-05-07 20:01:44 +0530

    Removed the test folder, Added two packages, screens and src, App now has a scaffolding and an Appbar

---
+++ to_do_application/lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_application/screens/home_screen.dart';
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
