4d022c2b45e2814e04312228e79fb375bbd9e8c4
Sidkjr <siddhant.khare.sk@gmail.com>
2023-05-07 22:30:34 +0530

    Added a Const type to Scaffold Text title

--- to_do_application/lib/screens/home_screen.dart
+++ to_do_application/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -9,7 +10,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("To Do App"),
+        title: const Text("To Do App"),
       ),
     );
   }
