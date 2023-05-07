05015d00795ea2da4247ddf7ffb6db31d8c2722a
Sidkjr <siddhant.khare.sk@gmail.com>
2023-05-07 19:15:59 +0530

    Modified the Body, Added Image, ListTile used for Name and About, Elevated Button for Two Actions, Fight & Message

--- profile_ui_application/lib/app.dart
+++ profile_ui_application/lib/app.dart
@@ -7,10 +7,31 @@ class App extends StatelessWidget {
       theme:
           ThemeData(appBarTheme: const AppBarTheme(color: Colors.greenAccent)),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Profile UI"),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text("Profile UI"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Image.asset("images/madara.jpg", width: 200, height: 200),
+              const ListTile(
+                title: Text("Madara Uchiha"),
+                subtitle: Text("The Ghost of Uchiha"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Message"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Fight Me"),
+              ),
+              const ListTile(
+                title: Text("About Me"),
+                subtitle: Text(
+                    "An Elite Ninja, Brother of Hashirama who was the first Hokage of the village hidden in the leaves."),
+              )
+            ],
+          )),
     );
   }
 }
