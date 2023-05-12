993b06c333ec955d8d77ce242ff6ce9a22185ccf
Sidkjr <siddhant.khare.sk@gmail.com>
2023-05-12 17:15:56 +0530

    Added a Categories Screen and a theme of orange in the app. Added a Elevated Button which allows us to go back to HomeScreen

--- to_do_application/lib/screens/categories_screen.dart
+++ to_do_application/lib/screens/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_application/screens/home_screen.dart';
+import 'package:to_do_application/src/app.dart';
 
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -10,9 +12,28 @@ class CategoriesScreen extends StatefulWidget {
 class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text("Welcome to Categories Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        leading: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.orangeAccent
+          ),
+          
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
+          child: ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()));
+              },
+              icon: Icon(Icons.arrow_back), label: Text(""),),
+        ),
+        
+        title: Text('El Todo'),
+        
+      ),
+      body: Center(
+        child: Text("Welcome to Categories Screen!"),
       ),
     );
   }
